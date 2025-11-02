88a51529b4e2d032c505d549208137224456a298
Coding <codingmaniac227@gmail.com>
2025-11-02 00:50:19 -0400

    refactor(TodoForm): revamp form to a more controlled form

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 
 export default function TodoForm({ onAddTodo }) {
+    const [ workingTodoTitle, setWorkingTodoTitle ] = React.useState('')
+
     const inputRef = React.useRef();
 
         const handleAddTodo = (event) => {
-        event.preventDefault();
+            event.preventDefault();
 
 
-        console.log(`Event object: ${event}`)
-        console.log(`Event target: ${event.target}`);
-        console.log(`Input value: ${event.target.todoTitle.value}`)
+            console.log(`Event object: ${event}`)
+            console.log(`Event target: ${event.target}`);
+            console.log(`Input value: ${event.target.value}`)
 
-        const todoTitle = event.target.todoTitle.value.trim()
-        if (todoTitle) {
-            onAddTodo(todoTitle)
-            event.target.reset()
-            inputRef.current.focus()
-        }
+            if (workingTodoTitle) {
+                onAddTodo(workingTodoTitle)
+                setWorkingTodoTitle('')
+                inputRef.current.focus()
+            }
     }
 
     return (
@@ -28,9 +29,11 @@ export default function TodoForm({ onAddTodo }) {
                     id="todoTitle"
                     name="todoTitle"
                     ref={inputRef}
+                    value={workingTodoTitle}
+                    onChange={(event) => {setWorkingTodoTitle(event.target.value)}}
                     required
                 />
-                <button type="submit">Add Todo</button>
+                <button type="submit" disabled={workingTodoTitle === '' ? true : null}>Add Todo</button>
             </form>
         </>
     )
