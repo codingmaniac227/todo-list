72b4f2203cd3fd7ec640b7a0ea4bdb13ae21b931
Coding <codingmaniac227@gmail.com>
2025-11-01 22:36:47 -0400

    feat(TodoForm): add ref to form, handler for sending form data up to state, and logs to visualize event trigger state updates

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
 
-export default function TodoForm() {
+export default function TodoForm({ onAddTodo }) {
+    const inputRef = React.useRef();
+
+        const handleAddTodo = (event) => {
+        event.preventDefault();
+
+
+        console.log(`Event object: ${event}`)
+        console.log(`Event target: ${event.target}`);
+        console.log(`Input value: ${event.target.todoTitle.value}`)
+
+        const todoTitle = event.target.todoTitle.value.trim()
+        if (todoTitle) {
+            onAddTodo(todoTitle)
+            event.target.reset()
+            inputRef.current.focus()
+        }
+    }
+
     return (
         <>
-            <form>
+            <form onSubmit={handleAddTodo}>
                 <label htmlFor="todoTitle">Todo</label>
-                <input type="text" id="todoTitle" />
-                <button type="submit" disabled>Add Todo</button>
+                <input
+                    type="text"
+                    id="todoTitle"
+                    name="todoTitle"
+                    ref={inputRef}
+                    required
+                />
+                <button type="submit">Add Todo</button>
             </form>
         </>
     )
